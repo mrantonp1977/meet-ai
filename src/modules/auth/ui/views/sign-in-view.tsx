86a495f7f1a36555e47c9142c0aabab65b9901b5
Antonis Papaioannou <mrantonp@gmail.com>
2025-06-01 00:43:17 +0300

    feat: add Dicebear avatar generation and integrate into dashboard user button feat: implement layout and page structure for dashboard with authentication checks feat: update global styles for sidebar and primary colors

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -181,7 +181,7 @@ export const SignInView = () => {
               </div>
             </form>
           </Form>
-          <div className="bg-radial from-green-400 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <Image
               src={'/logo.svg'}
               alt="Meet.AI Logo"
